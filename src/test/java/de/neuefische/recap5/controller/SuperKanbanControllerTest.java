bd07d90e3c3e9bec6b811080fe413101fa5841d6
steven <steven.mandau@gmx.de>
2024-02-22 11:53:46 +0100

    added integration test for getAllTasks

---
+++ src/test/java/de/neuefische/recap5/controller/SuperKanbanControllerTest.java
@@ -0,0 +1,34 @@
+package de.neuefische.recap5.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@RequiredArgsConstructor
+class SuperKanbanControllerTest {
+
+    @Autowired
+    private final MockMvc mvc;
+    @Test
+    void getAllTasks() throws Exception {
+        // GIVEN
+        // WHEN
+        mvc.perform(MockMvcRequestBuilders.get("api/todo"))
+        // THEN
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                    []
+                """));
+
+    }
+}
